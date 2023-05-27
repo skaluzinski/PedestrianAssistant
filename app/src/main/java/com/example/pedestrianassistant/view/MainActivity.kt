5ac1fb9f01766c6022c0953a6227f5df7ae9cf96
skaluzinski1801 <skaluzinski1801@gmail.com>
2023-05-27 19:33:22 +0200

    commit to move changes to remote

--- app/src/main/java/com/example/pedestrianassistant/view/MainActivity.kt
+++ app/src/main/java/com/example/pedestrianassistant/view/MainActivity.kt
@@ -1,264 +1,172 @@
 package com.example.pedestrianassistant.view
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.mlkit.vision.MlKitAnalyzer
-import androidx.camera.view.CameraController
-import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
-import androidx.camera.view.LifecycleCameraController
+import androidx.annotation.RequiresApi
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.lifecycle.ProcessCameraProvider.getInstance
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import com.example.pedestrianassistant.ui.theme.PedestrianAssistantTheme
-import com.example.pedestrianassistant.view.navigation.NavRoutes
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import androidx.room.jarjarred.org.stringtemplate.v4.Interpreter
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tflite.java.TfLite
+import com.google.common.util.concurrent.ListenableFuture
 import dagger.hilt.android.AndroidEntryPoint
-import io.eyram.iconsax.IconSax
+import java.io.FileDescriptor
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val initializeTask: Task<Void> by lazy {
+        TfLite.initialize(this).addOnFailureListener{
+            Log.e("TFLITE", it.message.toString())
+        }
+    }
+//    private val objectDetectorHelper by lazy {
+//        ObjectDetectorHelper(applicationContext,0.5f, 2, 3, 0, "mobilenetWithMetadata.tflite")
+//    }
+
+    private var objectDetectorHelper: ObjectDetectorHelper? = null
+    private lateinit var interpreter: Interpreter
+
     companion object {
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CAMERA_PERMISSION = 1001
+        private const val TAG = "CameraX"
     }
 
-    private fun allPermissionsGranted() =
-        REQUIRED_PERMISSIONS.all {
-            ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-        }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                Log.i("perm", "Permission granted")
-                shouldShowCamera.value = true //
-            } else {
-                Log.i("perm", "Permission denied")
-            }
-        }
-
-    private fun requestCameraPermission() {
-        when {
-            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
-                PackageManager.PERMISSION_GRANTED -> {
-                Log.i("perm", "Permission previously granted")
-                shouldShowCamera.value = true // 👈🏽
-            }
-            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) ->
-                Log.i("perm", "Show camera permissions dialog")
-            else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-        }
-    }
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PedestrianAssistantTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting()
-                }
-            }
-        }
-        requestCameraPermission()
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Greeting() {
-    var tabIndex by remember { mutableStateOf(0) }
-
-    val tabs = listOf(NavRoutes.TrainingScreen, NavRoutes.DetectingScreen)
-    Scaffold(
-        content = { innerPadding ->
-            Column(
-                Modifier.fillMaxSize().padding(bottom = innerPadding.calculateBottomPadding()),
-            ) {
-                Column(modifier = Modifier.weight(1f)) {
-                    when (tabIndex) {
-                        0 -> TrainingScreen()
-                        1 -> CameraView({},
-                        )
-                    }
-                }
-                TabRow(selectedTabIndex = tabIndex) {
-                    tabs.forEachIndexed { index, navRoute ->
-                        Tab(
-                            text = { Text(text = navRoute.route) },
-                            selected = tabIndex == index,
-                            onClick = { tabIndex = index },
-                            icon = {
-                                when (index) {
-                                    0 ->
-                                        Icon(
-                                            imageVector = Icons.Default.Home,
-                                            contentDescription = null
-                                        )
-                                    1 ->
-                                        Icon(
-                                            imageVector = Icons.Default.Info,
-                                            contentDescription = null
-                                        )
-                                }
-                            }
-                        )
-                    }
-                }
-            }
-        }
-    )
-}
-
-@Composable
-fun TrainingScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Work in progress", modifier = Modifier.align(Alignment.Center))
-    }
-}
-
-@Composable
-fun CameraView(
-    onInputVinClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
-
-    Box(modifier = modifier) {
-        AndroidView(
-            modifier =
-                Modifier.fillMaxSize().drawWithContent {
-                    drawContent()
-                },
-            factory = {
-                PreviewView(it).apply {
-                    controller =
-                        getVinRecognizingCamera(
-                            context = context,
-                            lifecycleOwner = lifecycleOwner
-                        )
-                }
-            },
-        )
-
-        Button(
-            modifier = Modifier.align(Alignment.BottomCenter),
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors(),
-            onClick = onInputVinClick
-        ) {
-            Text(modifier = Modifier.padding(16.dp), text = "Input VIN")
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun VinInputField(
-    vinInput: String,
-    onScanClick: () -> Unit,
-    onInputChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier.padding(32.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        OutlinedTextField(
-            modifier = Modifier.weight(1f),
-            value = vinInput,
-            onValueChange = onInputChange,
-            shape = RoundedCornerShape(8.dp),
-            colors =
-                TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color(0xFFFF8A65),
-                    unfocusedIndicatorColor = Color.LightGray
-                )
-        )
-
-        IconButton(
-            modifier =
-                Modifier.background(color = Color(0xFFFF8A65), shape = RoundedCornerShape(8.dp)),
-            onClick = onScanClick
-        ) {
-            Icon(
-                painterResource(id = IconSax.TwoTone.Scan),
-                contentDescription = null,
-                tint = Color.White
+        initializeTask.addOnSuccessListener {
+            Log.d("TFLITE", "TensorFlow Lite initialization successful")
+            objectDetectorHelper = ObjectDetectorHelper(
+                applicationContext,
+                0.5f,
+                2,
+                3,
+                "model.tflite"
             )
+            if (allPermissionsGranted()) {
+                setContent { MainContent() }
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CAMERA_PERMISSION
+                )
+            }
+        }.addOnFailureListener { e ->
+            Log.e("TFLITE", "TensorFlow Lite initialization failed: ${e.message}")
+        }
+    }
+
+    private fun allPermissionsGranted() = arrayOf(Manifest.permission.CAMERA).all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @Deprecated("Deprecated in Java")
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CAMERA_PERMISSION && allPermissionsGranted()) {
+            setContent {
+                MainContent()
+            }
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    @Composable
+    private fun MainContent() {
+        Column(modifier = Modifier.padding(16.dp).fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "CameraX with Compose", fontSize = 24.sp, modifier = Modifier.padding(bottom = 8.dp))
+            CameraView()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    @Composable
+    private fun CameraView() {
+
+        val localContext = LocalContext.current
+        val previewView = remember { PreviewView(localContext) }
+        val cameraProviderFuture = remember { getInstance(localContext) }
+        val lifecycleOwner = LocalLifecycleOwner.current
+
+        val observer = remember(previewView, cameraProviderFuture) {
+            CameraXObserver(previewView, cameraProviderFuture)
+        }
+
+        AndroidView({ previewView }, Modifier.fillMaxSize()) {
+            lifecycleOwner.lifecycle.addObserver(observer)
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun initializeCameraProvider(previewView: PreviewView, cameraProvider: ProcessCameraProvider) {
+
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        cameraProvider.unbindAll()
+
+        val preview = Preview.Builder().build().apply { setSurfaceProvider(previewView.surfaceProvider) }
+
+        val imageAnalyzer = ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build().apply {
+            setAnalyzer(ContextCompat.getMainExecutor(previewView.context)) { imageProxy ->
+                processImageProxy(imageProxy)
+            }
+        }
+
+        cameraProvider.bindToLifecycle(previewView.context as ComponentActivity, cameraSelector, preview, imageAnalyzer)
+    }
+
+    private fun processImageProxy(image: ImageProxy) {
+        image.use {
+            val bitmap = image.toBitmap()
+            val results = objectDetectorHelper!!.detect(bitmap, image.imageInfo.rotationDegrees)
+            Log.i(TAG, results.toString())
+        }
+    }
+
+    inner class CameraXObserver(private val previewView: PreviewView, private val cameraProviderFuture: ListenableFuture<ProcessCameraProvider>) : LifecycleEventObserver {
+
+        @RequiresApi(Build.VERSION_CODES.P)
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> cameraProviderFuture.addListener({ initializeCameraProvider(previewView, cameraProviderFuture.get()) }, ContextCompat.getMainExecutor(previewView.context))
+                Lifecycle.Event.ON_PAUSE -> cameraProviderFuture.get().unbindAll()
+                else -> Unit
+            }
         }
     }
 }
-
-fun getVinRecognizingCamera(
-    context: Context,
-    lifecycleOwner: LifecycleOwner
-): CameraController {
-    val textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    val vinRegex =
-        Regex(pattern = """\b[(A-H|J-N|P|R-Z|0-9)]{17}\b""", options = setOf(RegexOption.MULTILINE))
-
-    return LifecycleCameraController(context).apply {
-        setImageAnalysisAnalyzer(
-            ContextCompat.getMainExecutor(context),
-            MlKitAnalyzer(
-                listOf(textRecognizer),
-                COORDINATE_SYSTEM_VIEW_REFERENCED,
-                ContextCompat.getMainExecutor(context)
-            ) { analyzerResult ->
-                analyzerResult.getValue(textRecognizer)?.let { text ->
-
-
-                    val recognizedVins =
-                        text.textBlocks
-                            .flatMap { textBlock -> textBlock.lines }
-                            .mapNotNull { lines ->
-                                lines
-                                    .takeIf { vinRegex.matches(it.text) }
-                                    ?.let { RecognizedVin(it.text, it.boundingBox) }
-                            }
-
-                }
-            }
-        )
-        bindToLifecycle(lifecycleOwner)
-    }
-}
-
-data class RecognizedVin(
-    val text: String,
-    val boundingBox: android.graphics.Rect?,
-)
--- app/src/main/java/com/example/pedestrianassistant/view/ObjectDetectorHelper.kt
+++ app/src/main/java/com/example/pedestrianassistant/view/ObjectDetectorHelper.kt
@@ -1,4 +1,58 @@
 package com.example.pedestrianassistant.view
 
-class ObjectDetectorHelper {
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.Rot90Op
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.gms.vision.detector.Detection
+import org.tensorflow.lite.task.gms.vision.detector.ObjectDetector
+
+class ObjectDetectorHelper(
+    private val context: Context,
+    private var threshold: Float = 0.5f,
+    private var numThreads: Int = 2,
+    private var maxResults: Int = 3,
+    private var currentModel: String
+) {
+
+    private val GPU_DELEGATE_ERROR = "GPU is not supported on this device"
+    private val MODEL_LOADING_ERROR = "TFLite failed to load model with error: "
+
+    private lateinit var objectDetector: ObjectDetector
+
+    init {
+        setupObjectDetector()
+    }
+
+    private fun setupObjectDetector() {
+
+        val optionsBuilder = ObjectDetector
+            .ObjectDetectorOptions
+            .builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(numThreads)
+
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+        try {
+            objectDetector = ObjectDetector.createFromFileAndOptions(context, currentModel, optionsBuilder.build())
+        } catch (e: IllegalStateException) {
+            Log.e("Test", MODEL_LOADING_ERROR + e.message)
+        }
+    }
+
+    fun detect(image: Bitmap, imageRotation: Int): DetectionResults {
+        if (!::objectDetector.isInitialized) setupObjectDetector()
+        val imageProcessor = ImageProcessor.Builder().add(Rot90Op(-imageRotation / 90)).build()
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(image))
+        val results = objectDetector.detect(tensorImage)
+
+        return DetectionResults(results, tensorImage.height, tensorImage.width)
+    }
+
+    data class DetectionResults(val results: MutableList<Detection>?, val height: Int, val width: Int)
 }
