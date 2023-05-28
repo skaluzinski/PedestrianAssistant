480b2c17736d93ee948447315ac37ab129f778e1
skaluzinski1801 <skaluzinski1801@gmail.com>
2023-05-28 21:27:48 +0200

    feat: created base project using mobilenetV1 model to detect what's currently shown on live camera

--- app/src/main/java/com/example/pedestrianassistant/view/MainActivity.kt
+++ app/src/main/java/com/example/pedestrianassistant/view/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.pedestrianassistant.view
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
+import androidx.camera.core.*
+import androidx.camera.core.ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.lifecycle.ProcessCameraProvider.getInstance
 import androidx.camera.view.PreviewView
@@ -21,6 +20,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,30 +30,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.room.jarjarred.org.stringtemplate.v4.Interpreter
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.tflite.java.TfLite
 import com.google.common.util.concurrent.ListenableFuture
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.FileDescriptor
+import org.tensorflow.lite.task.gms.vision.detector.Detection
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
-    private val initializeTask: Task<Void> by lazy {
-        TfLite.initialize(this).addOnFailureListener{
-            Log.e("TFLITE", it.message.toString())
-        }
-    }
-//    private val objectDetectorHelper by lazy {
-//        ObjectDetectorHelper(applicationContext,0.5f, 2, 3, 0, "mobilenetWithMetadata.tflite")
-//    }
+class MainActivity : ComponentActivity(),ObjectDetectorHelper.DetectorListener {
 
-    private var objectDetectorHelper: ObjectDetectorHelper? = null
-    private lateinit var interpreter: Interpreter
+    private lateinit var objectDetectorHelper: ObjectDetectorHelper
+    private lateinit var bitmapBuffer: Bitmap
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    private var detectedObject = mutableStateOf("Nothing")
+    /** Blocking camera operations are performed using this executor */
+    private var cameraExecutor: ExecutorService  = Executors.newSingleThreadExecutor()
 
     companion object {
         private const val REQUEST_CAMERA_PERMISSION = 1001
@@ -63,52 +63,53 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeTask.addOnSuccessListener {
-            Log.d("TFLITE", "TensorFlow Lite initialization successful")
-            objectDetectorHelper = ObjectDetectorHelper(
-                applicationContext,
-                0.5f,
-                2,
-                3,
-                "model.tflite"
+        Log.d("TFLITE", "TensorFlow Lite initialization successful")
+        objectDetectorHelper =
+            ObjectDetectorHelper(context = applicationContext, objectDetectorListener = this)
+        if (allPermissionsGranted()) {
+            setContent { MainContent() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
             )
-            if (allPermissionsGranted()) {
-                setContent { MainContent() }
-            } else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.CAMERA),
-                    REQUEST_CAMERA_PERMISSION
-                )
-            }
-        }.addOnFailureListener { e ->
-            Log.e("TFLITE", "TensorFlow Lite initialization failed: ${e.message}")
         }
     }
 
-    private fun allPermissionsGranted() = arrayOf(Manifest.permission.CAMERA).all {
-        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
-    }
+    private fun allPermissionsGranted() =
+        arrayOf(Manifest.permission.CAMERA).all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
 
     @Deprecated("Deprecated in Java")
     @RequiresApi(Build.VERSION_CODES.P)
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (requestCode == REQUEST_CAMERA_PERMISSION && allPermissionsGranted()) {
-            setContent {
-                MainContent()
-            }
+            setContent { MainContent() }
         }
-
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
     @Composable
     private fun MainContent() {
-        Column(modifier = Modifier.padding(16.dp).fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "CameraX with Compose", fontSize = 24.sp, modifier = Modifier.padding(bottom = 8.dp))
+        Column(
+            modifier = Modifier.padding(16.dp).fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = detectedObject.value,
+                fontSize = 24.sp,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
             CameraView()
         }
     }
@@ -122,51 +123,108 @@ class MainActivity : ComponentActivity() {
         val cameraProviderFuture = remember { getInstance(localContext) }
         val lifecycleOwner = LocalLifecycleOwner.current
 
-        val observer = remember(previewView, cameraProviderFuture) {
-            CameraXObserver(previewView, cameraProviderFuture)
-        }
+        val observer =
+            remember(previewView, cameraProviderFuture) {
+                CameraXObserver(previewView, cameraProviderFuture)
+            }
 
         AndroidView({ previewView }, Modifier.fillMaxSize()) {
             lifecycleOwner.lifecycle.addObserver(observer)
         }
-
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
-    private fun initializeCameraProvider(previewView: PreviewView, cameraProvider: ProcessCameraProvider) {
+    private fun initializeCameraProvider(
+        previewView: PreviewView,
+        cameraProvider: ProcessCameraProvider
+    ) {
 
-        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
         cameraProvider.unbindAll()
 
-        val preview = Preview.Builder().build().apply { setSurfaceProvider(previewView.surfaceProvider) }
+        val preview =
+            Preview.Builder().build().apply { setSurfaceProvider(previewView.surfaceProvider) }
 
-        val imageAnalyzer = ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build().apply {
-            setAnalyzer(ContextCompat.getMainExecutor(previewView.context)) { imageProxy ->
-                processImageProxy(imageProxy)
-            }
-        }
+        val imageAnalyzer =
+            ImageAnalysis.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                //
+                // .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    it.setAnalyzer(cameraExecutor) { image ->
+                        if (!::bitmapBuffer.isInitialized) {
+                            // The image rotation and RGB image buffer are initialized only once
+                            // the analyzer has started running
+                            bitmapBuffer =
+                                Bitmap.createBitmap(
+                                    image.width,
+                                    image.height,
+                                    Bitmap.Config.ARGB_8888
+                                )
+                        }
 
-        cameraProvider.bindToLifecycle(previewView.context as ComponentActivity, cameraSelector, preview, imageAnalyzer)
+                        detectObjects(image)
+                    }
+                }
+        cameraProvider.bindToLifecycle(
+            previewView.context as ComponentActivity,
+            cameraSelector,
+            preview,
+            imageAnalyzer
+        )
     }
 
-    private fun processImageProxy(image: ImageProxy) {
-        image.use {
-            val bitmap = image.toBitmap()
-            val results = objectDetectorHelper!!.detect(bitmap, image.imageInfo.rotationDegrees)
-            Log.i(TAG, results.toString())
-        }
+    private fun detectObjects(image: ImageProxy) {
+        // Copy out RGB bits to the shared bitmap buffer
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+
+        val imageRotation = image.imageInfo.rotationDegrees
+        // Pass Bitmap and rotation to the object detector helper for processing and detection
+        objectDetectorHelper.detect(bitmapBuffer, imageRotation)
     }
 
-    inner class CameraXObserver(private val previewView: PreviewView, private val cameraProviderFuture: ListenableFuture<ProcessCameraProvider>) : LifecycleEventObserver {
+    inner class CameraXObserver(
+        private val previewView: PreviewView,
+        private val cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    ) : LifecycleEventObserver {
 
         @RequiresApi(Build.VERSION_CODES.P)
         override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
             when (event) {
-                Lifecycle.Event.ON_RESUME -> cameraProviderFuture.addListener({ initializeCameraProvider(previewView, cameraProviderFuture.get()) }, ContextCompat.getMainExecutor(previewView.context))
+                Lifecycle.Event.ON_RESUME ->
+                    cameraProviderFuture.addListener(
+                        { initializeCameraProvider(previewView, cameraProviderFuture.get()) },
+                        ContextCompat.getMainExecutor(previewView.context)
+                    )
                 Lifecycle.Event.ON_PAUSE -> cameraProviderFuture.get().unbindAll()
                 else -> Unit
             }
         }
+    }
+
+    override fun onInitialized() {
+        objectDetectorHelper.setupObjectDetector()
+        // Initialize our background executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onError(error: String) {
+        Log.d("RESULTS-ERROR",error)
+    }
+
+    override fun onResults(
+        results: MutableList<Detection>?,
+        inferenceTime: Long,
+        imageHeight: Int,
+        imageWidth: Int
+    ) {
+        detectedObject.value = if (results?.isNotEmpty() == true) results.first().categories.first().label.toString() else "Notghing"
+        Log.d("RESULTS", results.toString())
     }
 }
--- app/src/main/java/com/example/pedestrianassistant/view/ObjectDetectorHelper.kt
+++ app/src/main/java/com/example/pedestrianassistant/view/ObjectDetectorHelper.kt
@@ -2,57 +2,163 @@ package com.example.pedestrianassistant.view
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.os.SystemClock
 import android.util.Log
+import com.google.android.gms.tflite.client.TfLiteInitializationOptions
+import com.google.android.gms.tflite.gpu.support.TfLiteGpu
+import org.tensorflow.lite.support.common.ops.NormalizeOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.support.image.ops.Rot90Op
 import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.gms.vision.TfLiteVision
 import org.tensorflow.lite.task.gms.vision.detector.Detection
 import org.tensorflow.lite.task.gms.vision.detector.ObjectDetector
 
 class ObjectDetectorHelper(
-    private val context: Context,
-    private var threshold: Float = 0.5f,
-    private var numThreads: Int = 2,
-    private var maxResults: Int = 3,
-    private var currentModel: String
+    var threshold: Float = 0.5f,
+    var numThreads: Int = 2,
+    var maxResults: Int = 3,
+    var currentDelegate: Int = 0,
+    var currentModel: Int = 0,
+    val context: Context,
+    val objectDetectorListener: DetectorListener
 ) {
 
-    private val GPU_DELEGATE_ERROR = "GPU is not supported on this device"
-    private val MODEL_LOADING_ERROR = "TFLite failed to load model with error: "
+    private val TAG = "ObjectDetectionHelper"
 
-    private lateinit var objectDetector: ObjectDetector
+    // For this example this needs to be a var so it can be reset on changes. If the ObjectDetector
+    // will not change, a lazy val would be preferable.
+    private var objectDetector: ObjectDetector? = null
+    private var gpuSupported = false
 
     init {
-        setupObjectDetector()
-    }
 
-    private fun setupObjectDetector() {
-
-        val optionsBuilder = ObjectDetector
-            .ObjectDetectorOptions
-            .builder()
-            .setScoreThreshold(threshold)
-            .setMaxResults(maxResults)
-
-        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(numThreads)
-
-        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
-        try {
-            objectDetector = ObjectDetector.createFromFileAndOptions(context, currentModel, optionsBuilder.build())
-        } catch (e: IllegalStateException) {
-            Log.e("Test", MODEL_LOADING_ERROR + e.message)
+        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable: Boolean ->
+            val optionsBuilder =
+                TfLiteInitializationOptions.builder()
+            if (gpuAvailable) {
+                optionsBuilder.setEnableGpuDelegateSupport(true)
+            }
+            TfLiteVision.initialize(context, optionsBuilder.build())
+        }.addOnSuccessListener {
+            objectDetectorListener.onInitialized()
+        }.addOnFailureListener{
+            objectDetectorListener.onError("TfLiteVision failed to initialize: "
+            + it.message)
         }
     }
 
-    fun detect(image: Bitmap, imageRotation: Int): DetectionResults {
-        if (!::objectDetector.isInitialized) setupObjectDetector()
-        val imageProcessor = ImageProcessor.Builder().add(Rot90Op(-imageRotation / 90)).build()
-        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(image))
-        val results = objectDetector.detect(tensorImage)
-
-        return DetectionResults(results, tensorImage.height, tensorImage.width)
+    fun clearObjectDetector() {
+        objectDetector = null
     }
 
-    data class DetectionResults(val results: MutableList<Detection>?, val height: Int, val width: Int)
+    // Initialize the object detector using current settings on the
+    // thread that is using it. CPU and NNAPI delegates can be used with detectors
+    // that are created on the main thread and used on a background thread, but
+    // the GPU delegate needs to be used on the thread that initialized the detector
+    fun setupObjectDetector() {
+        if (!TfLiteVision.isInitialized()) {
+            Log.e(TAG, "setupObjectDetector: TfLiteVision is not initialized yet")
+            return
+        }
+
+        // Create the base options for the detector using specifies max results and score threshold
+        val optionsBuilder =
+            ObjectDetector.ObjectDetectorOptions.builder()
+                .setScoreThreshold(threshold)
+                .setMaxResults(maxResults)
+
+        // Set general detection options, including number of used threads
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(numThreads)
+
+        // Use the specified hardware for running the model. Default to CPU
+        when (currentDelegate) {
+            DELEGATE_CPU -> {
+                // Default
+            }
+            DELEGATE_GPU -> {
+                if (gpuSupported) {
+                    baseOptionsBuilder.useGpu()
+                } else {
+                    objectDetectorListener.onError("GPU is not supported on this device")
+                }
+            }
+            DELEGATE_NNAPI -> {
+                baseOptionsBuilder.useNnapi()
+            }
+        }
+
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        val modelName = "lite-model_ssd_mobilenet_v1_1_metadata_2.tflite"
+
+        try {
+            objectDetector =
+                ObjectDetector.createFromFileAndOptions(context, modelName, optionsBuilder.build())
+        } catch (e: Exception) {
+            objectDetectorListener.onError(
+                "Object detector failed to initialize. See error logs for details"
+            )
+            Log.e(TAG, "TFLite failed to load model with error: " + e.message)
+        }
+    }
+
+    fun detect(image: Bitmap, imageRotation: Int) {
+        if (!TfLiteVision.isInitialized()) {
+            Log.e(TAG, "detect: TfLiteVision is not initialized yet")
+            return
+        }
+
+        if (objectDetector == null) {
+            setupObjectDetector()
+        }
+
+        // Inference time is the difference between the system time at the start and finish of the
+        // process
+        var inferenceTime = SystemClock.uptimeMillis()
+
+        // Create preprocessor for the image.
+        // See https://www.tensorflow.org/lite/inference_with_metadata/
+        //            lite_support#imageprocessor_architecture
+        val imageProcessor = ImageProcessor
+            .Builder()
+//            .add(ResizeOp(32,32,ResizeOp.ResizeMethod.BILINEAR))
+            .add(Rot90Op(-imageRotation / 90))
+//            .add(NormalizeOp(127.5f,127.5f))
+            .build()
+
+        // Preprocess the image and convert it into a TensorImage for detection.
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(image))
+
+        val results = objectDetector?.detect(tensorImage)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        objectDetectorListener.onResults(
+            results,
+            inferenceTime,
+            tensorImage.height,
+            tensorImage.width)
+    }
+
+    interface DetectorListener {
+        fun onInitialized()
+        fun onError(error: String)
+        fun onResults(
+            results: MutableList<Detection>?,
+            inferenceTime: Long,
+            imageHeight: Int,
+            imageWidth: Int
+        )
+    }
+
+    companion object {
+        const val DELEGATE_CPU = 0
+        const val DELEGATE_GPU = 1
+        const val DELEGATE_NNAPI = 2
+        const val MODEL_MOBILENETV1 = 0
+        const val MODEL_EFFICIENTDETV0 = 1
+        const val MODEL_EFFICIENTDETV1 = 2
+        const val MODEL_EFFICIENTDETV2 = 3
+    }
 }
